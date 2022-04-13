beb5903e91fabedcf2271f23e4e46a109e8a8ad4
이정환 <king199777@gmail.com>
2022-04-13 13:59:17 +0900

    Update index.js

--- contribute-handler/index.js
+++ contribute-handler/index.js
@@ -6,12 +6,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function run() {
+  console.log("🌱 1. get discussion data");
   const discussionData = getDiscussionData();
-  const contentData = getClassifiedText(discussionData.body);
+  console.log(discussionData + "\n\n");
 
+  console.log("🌱 2. classifiy contennt data");
+  const contentData = getClassifiedText(discussionData.body);
+  console.log(contentData + "\n\n");
+
+  console.log("🌱 3. get opengraph data by url");
   const ogData = await getOpengraph(contentData.content_url);
+  console.log(ogData + "\n\n");
+
+  console.log("🌱 4. post slack");
   const res = await postSlack(discussionData, contentData, ogData);
-  console.log(res);
+  console.log(res + "\n\n");
 }
 
 run();
