7ac69f1408d8afe9a7acd5042d4578fc695229a8
이정환 <king199777@gmail.com>
2022-04-21 13:06:30 +0900

    기고시 자동으로 포스트하는 Tweet bot 추가

--- contribute-handler/index.js
+++ contribute-handler/index.js
@@ -1,7 +1,8 @@
 import getDiscussionData from "./lib/get-discussion-data.js";
-import getOpengraph from "./lib/get-opengraph.js";
 import getClassifiedText from "./lib/get-classified-text.js";
+import getOpengraph from "./lib/get-opengraph.js";
 import postSlack from "./lib/post-slack.js";
+import postTweet from "./lib/post-tweet.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -18,9 +19,13 @@ async function run() {
   const ogData = await getOpengraph(contentData.content_url);
   console.log(JSON.stringify(ogData, null, 2), +"\n\n");
 
-  console.log("🌱 4. post slack");
-  const res = await postSlack(discussionData, contentData, ogData);
-  console.log(JSON.stringify(res, null, 2), +"\n\n");
+  console.log("🌱 4. post community channels");
+  const [slack_res, tweet_res] = await Promise.allSettled([
+    postSlack(discussionData, contentData, ogData),
+    postTweet(discussionData),
+  ]);
+  console.log(JSON.stringify(slack_res, null, 2), +"\n\n");
+  console.log(JSON.stringify(tweet_res, null, 2), +"\n\n");
 }
 
 run();
---
+++ contribute-handler/lib/post-tweet.js
@@ -0,0 +1,43 @@
+import Twitter from "twitter";
+
+export default async function postTweet(discussionData) {
+  const { number, category_name, url, title, created_at, user } =
+    discussionData;
+
+  const text = `
+  #개발자 #개발컨텐츠 #프로그래머
+
+  ${number}번째 기고
+
+  ${title}
+
+  ${url}
+`;
+
+  const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_API_KEY_SECRET,
+    access_token_key: process.env.TWITTER_PMING_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_PMING_ACCESS_TOKEN_SECRET,
+  });
+
+  return new Promise((resovle, reject) =>
+    client.post(
+      "statuses/update",
+      {
+        status: text,
+      },
+      function (error, tweet) {
+        if (!error) {
+          console.log("🟢 TWEET SUCCESS");
+          console.log(`https://twitter.com/pming_kr/status/${tweet.id}`);
+          resovle();
+        } else {
+          console.log("🔴 TWEET FAIL");
+          console.log(error);
+          reject();
+        }
+      }
+    )
+  );
+}
