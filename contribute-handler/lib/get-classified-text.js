ff2cdbd8e1f88622138a08825243770ad44dbb87
이정환 <king199777@gmail.com>
2022-04-28 10:47:26 +0900

    []() 형식에 앞 뒤 모두 링크일 경우 발생하는 링크 파싱 오류 수정

--- contribute-handler/lib/get-classified-text.js
+++ contribute-handler/lib/get-classified-text.js
@@ -1,11 +1,24 @@
 function getUrl(text) {
+  // 마크다운 일 경우
+  const markDownURLRegex = /\[([^\[]+)\](\(.*\))/gm;
+  const mdMatches = text.match(markDownURLRegex);
+  if (mdMatches && mdMatches.length > 0) {
+    const singleMatch = /\[([^\[]+)\]\((.*)\)/;
+    let res = singleMatch.exec(mdMatches[0]);
+    return res[2];
+  }
+
+  // 일반 링크일 경우
   const urlRegex = /(https?:\/\/[^\s]+)/g;
   const urlList = text.match(urlRegex);
-  if (urlList.length > 0) {
+  if (urlList && urlList.length > 0) {
     let content_url = urlList[0];
-    if (content_url[content_url.length - 1] === ")") {
-      content_url = content_url.slice(0, -1);
-    }
+
+    if (content_url.includes("["))
+      if (content_url[content_url.length - 1] === ")") {
+        content_url = content_url.slice(0, -1);
+      }
+
     return content_url;
   } else {
     return "";
