bd436fea958dabaf4b36a2c111d52203b4c11ff4
이정환 <king199777@gmail.com>
2022-05-06 14:06:51 +0900

    페이스북 포스팅에 기고자 깃허브 주소 추가

--- contribute-handler/lib/post-slack-for-zapier.js
+++ contribute-handler/lib/post-slack-for-zapier.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function postSlackForZapier(discussionData, contentData) {
   const SLACK_WEBHOOK_URL = process.env.JAPIER_ALARM_WEBHOOK_URL;
 
-  const { id, number, title } = discussionData;
+  const { id, number, title, user } = discussionData;
   const { descript } = contentData;
 
   const pming_url = `https://www.pming.kr/c/${id}?utm_source=fb_pming`;
@@ -13,12 +13,12 @@ export default async function postSlackForZapier(discussionData, contentData) {
       `[ 요약 ]` +
       descript
         .replaceAll("#", "")
-        .replaceAll("TL;DR", "TL;DR\n")
-        .replaceAll("RECOMMEND", "[ 추천 대상 ]\n")
-        .replaceAll("EARN", "[ 느낀점 ]\n");
+        .replaceAll("TL;DR", "\nTL;DR\n")
+        .replaceAll("RECOMMEND", "\n[ 추천 대상 ]\n")
+        .replaceAll("EARN", "\n[ 느낀점 ]\n");
 
     const data = {
-      text: `${number}번째 '핸드픽'\n${title}\n\n${pming_url}\n\n${postDescript}`,
+      text: `${number}번째 '핸드픽'\n${title}\n\n${pming_url}\n\n${postDescript}\n\n[ 핸드 피커 ]\n${user.url}`,
     };
     const res = await axios
       .post(SLACK_WEBHOOK_URL, data)
